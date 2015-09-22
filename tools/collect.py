131185a4deab09bce58ee3bd7222752d615f573f
chtq <chtq199644@126.com>
2015-09-22 11:15:17 -0400

    update collect.py

--- tools/collect.py
+++ tools/collect.py
@@ -17,10 +17,16 @@ finally:
     if len(sys.argv) !=7:
        print "usage:%s %s %s %s %s %s %s" %(sys.argv[0], "<filepath>", "<1-5>", "<str>", "<str>", "<str>", "<str>")  
        exit(-1)
-    
+    count=0
+    pa1=sys.argv[2].strip('"').split(',')
+    pa2=sys.argv[3].strip('"').split(',')
+    pa3=sys.argv[4].strip('"').split(',')
+    pa4=sys.argv[5].strip('"').split(',')
+    pa5=sys.argv[6].strip('"').split(',')
+    print pa1,pa2,pa3,pa4,pa5
     for parent,dirnames,filenames in os.walk(sys.argv[1]):
         for filename in filenames:
-            if filename.find(".md")==-1 or filename=='examples.md':
+            if filename.find(".md")==-1 or filename=='examples.md' or filename.find(".md~")!=-1:
                continue
             else:
                i=""
@@ -34,16 +40,16 @@ finally:
    
                
                for line in fr.readlines():   
-                   if re.search(u'^(- \[)', line.decode('utf8'))!=None or re.search(u'^(- \()', line.decode('utf8'))!=None:
+                   if re.search(u'^(- \[)', line)!=None or re.search('^(- \()', line)!=None:
                        # print line
                         xx+=line.strip('\n')
                    elif re.search('^(> 出处：)', line)!=None:
                         #print cc
                         cc=line.strip('\n')
-                        print filename
+    
                    elif re.search('^(> 知识点：)', line)!=None:
                         kn=line.strip('\n')
-                        
+                        #print kn
                    elif re.search('> ', line)!=None:
                         js+=line.strip('\n')
                    elif re.search('^(\d\n)$', line)!=None:
@@ -55,33 +61,51 @@ finally:
                #print cc
                fr.close()
                if sys.argv[2]!='""':
-                  if i==sys.argv[2]:
-                       result=1
-                  else:
-                       continue
-              
+                  flag=0
+                  for j in range(len(pa1)):
+                    if i.find(pa1[j])!=-1:
+                        flag=1
+                        result=1
+                  if flag==0:
+                    continue
+
                if sys.argv[3]!='""':
-                  if tg.find(sys.argv[3])!=-1:
-                       result=1
-                  else:
-                       continue
+                  flag=0
+                  for j in range(len(pa2)):
+                    if tg.find(pa2[j])!=-1:
+                        flag=1
+                        result=1
+                  if flag==0:
+                    continue
                if sys.argv[4]!='""':
-                  if xx.find(sys.argv[4])!=-1:
-                       result=1
-                  else:
-                       continue
+                  flag=0
+                  for j in range(len(pa3)):
+                    if xx.find(pa3[j])!=-1:
+                        flag=1
+                        result=1
+                  if flag==0:
+                    continue
                if sys.argv[5]!='""':
-                  if js.find(sys.argv[5])!=-1:
-                       result=1
-                  else:
-                       continue
+                  flag=0
+                  for j in range(len(pa4)):
+                    if js.find(pa4[j])!=-1:
+                        flag=1
+                        result=1
+                  if flag==0:
+                    continue
                if sys.argv[6]!='""':
-                  if cc.find(sys.argv[6])!=-1:
-                       result=1
-                  else:
-                       continue
-         
+                  flag=0
+                  for j in range(len(pa5)):
+                    if cc.find(pa5[j])!=-1:
+                        flag=1
+                        result=1
+                  if flag==0:
+                    continue
+
                if result==1:
-                  shutil.copyfile(parent+'/'+filename, "./result/"+filename)   
+                  count=count+1
+                  print filename
+                  shutil.copyfile(parent+'/'+filename, "./result/"+filename)
+    print count,"个文件" 
                        
                    
